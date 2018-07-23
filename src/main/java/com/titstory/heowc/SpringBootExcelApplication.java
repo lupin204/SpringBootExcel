ed073adf1cc724e5ade82ea835693bc1dd880b0e
Lee Young Seo <lupin204@gmail.com>
2018-07-23 19:19:18 +0900

    first

---
+++ src/main/java/com/titstory/heowc/SpringBootExcelApplication.java
@@ -0,0 +1,14 @@
+package com.titstory.heowc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+
+@SpringBootApplication
+public class SpringBootExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExcelApplication.class, args);
+	}
+}
